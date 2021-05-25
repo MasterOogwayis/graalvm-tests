aac3583d030c9ba8af4a1bb4178b4e130f7d3128
zsw <499504777@qq.com>
2021-05-25 10:39:35 +0800

    new project

---
+++ simple-app/src/main/java/com/zsw/demo/GraalvmApp.java
@@ -0,0 +1,30 @@
+package com.zsw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZhangShaowei on 2021/5/25 10:16
+ */
+@RestController
+@SpringBootApplication(proxyBeanMethods = false)
+public class GraalvmApp {
+
+    /**
+     * start server
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GraalvmApp.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello(@RequestParam("name") String name) {
+        return "Hello " + name + "!";
+    }
+
+}
